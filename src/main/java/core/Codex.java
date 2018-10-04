f5461889c05798ba1d73b868c1e2313a761ab0a2
Arthur Buliva <arthurbuliva@gmail.com>
2018-10-04 17:22:28 +0200

    Reducing visibility from Public to package private Cleanup of redundant objects

--- src/main/java/core/Codex.java
+++ src/main/java/core/Codex.java
@@ -8,7 +8,7 @@ import java.util.Base64;
 /**
  * String Encoder and decoder
  */
-public class Codex
+class Codex
 {
 
     /**
@@ -17,12 +17,11 @@ public class Codex
      * @param plainText The text to be encoded
      * @return The encoded text
      */
-    public static String encode(String plainText)
+    static String encode(String plainText)
     {
         byte[] encodedBytes = Base64.getEncoder().withoutPadding().encode(plainText.getBytes(StandardCharsets.UTF_8));
-        String encoded = new String(encodedBytes, Charset.forName("UTF-8"));
 
-        return encoded;
+        return new String(encodedBytes, Charset.forName("UTF-8"));
     }
 
     /**
@@ -31,12 +30,11 @@ public class Codex
      * @param encodedString The encoded string to be decoded
      * @return The decoded String
      */
-    public static String decode(String encodedString)
+    static String decode(String encodedString)
     {
         byte[] decodedBytes = Base64.getDecoder().decode(encodedString.getBytes());
-        String decoded = new String(decodedBytes, Charset.forName("UTF-8"));
 
-        return decoded;
+        return  new String(decodedBytes, Charset.forName("UTF-8"));
     }
 
     /**
@@ -45,15 +43,14 @@ public class Codex
      * @param text The String to encode to Sha256
      * @return The encoded string
      */
-    public static String sha256(String text)
+    static String sha256(String text)
     {
         try
         {
             MessageDigest digest = MessageDigest.getInstance("SHA3-512");
             byte[] hash = digest.digest(text.getBytes(StandardCharsets.UTF_8));
-            String encoded = Base64.getEncoder().withoutPadding().encodeToString(hash);
 
-            return encoded;
+            return Base64.getEncoder().withoutPadding().encodeToString(hash);
         }
         catch (Exception exception)
         {

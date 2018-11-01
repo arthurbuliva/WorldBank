64414deac2baac3dc795412818c77385bbd972f1
Arthur Buliva <arthurbuliva@gmail.com>
2018-11-01 21:39:56 +0100

    New way of saving and retrieving the coins

--- src/main/java/test/CreateCoin.java
+++ src/main/java/test/CreateCoin.java
@@ -1,12 +1,17 @@
 package test;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import core.CoinDisplay;
 import core.Money;
 import currency.Kenya;
 
 import java.awt.*;
+import java.lang.reflect.Type;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 
 public class CreateCoin
@@ -18,6 +23,7 @@ public class CreateCoin
 
         HashMap<String, String> values = new HashMap();
         values.put("accountHolderName", "Mr Buliva!");
+//        values.put("accountHolderNamQe", "Mr Buliva!");
         values.put("accountNumber", "1234567890");
         values.put("accountHolderAddress", "Amsterdam, The Netherlands");
         values.put("taxCertificate", "d3WTByUmlLU1J0ZkxLQmF");
@@ -25,11 +31,37 @@ public class CreateCoin
 
         Money money = new Kenya(values);
 
+
+        System.out.println(money.validateValues());
+        System.out.println("***************");
+
+
         String coin = money.saveCoin();
 
+        System.out.println(coin);
+        System.out.println("###############");
 
-        System.out.println(display.showCoin(coin));
+        // Extract only the coin from the purse
 
+        String coinID = "";
 
+        Gson gson = new Gson();
+
+        Type collectionType = new TypeToken<Collection<HashMap>>(){}.getType();
+        Collection<HashMap> savedValues = gson.fromJson(coin, collectionType);
+
+        for(HashMap value : savedValues)
+        {
+            coinID = (String) value.get("coinId");
+
+            if(value.get("coinID") != null)
+            {
+                coinID = (String) value.get("coinID");
+
+                break;
+            }
+        }
+
+        System.out.println(display.showCoin(coinID));
     }
 }

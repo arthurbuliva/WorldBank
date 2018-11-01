9e05dad0ab410924cf41f163fc76454cd0d127ad
Arthur Buliva <arthurbuliva@gmail.com>
2018-11-01 21:40:29 +0100

    Optimizing

--- src/main/java/test/CreateCoin.java
+++ src/main/java/test/CreateCoin.java
@@ -6,11 +6,7 @@ import core.CoinDisplay;
 import core.Money;
 import currency.Kenya;
 
-import java.awt.*;
 import java.lang.reflect.Type;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 
@@ -21,7 +17,7 @@ public class CreateCoin
         CoinDisplay display = new CoinDisplay();
 //        System.out.println(display.showCoin("VXhvTmd3WTByUmlLU1J0ZkxLQmFKY28xa0lCVEtPUHg2Vm9ma1h0eHVFVzFoOStaRVZUbThqNGlGQ3NjTGRvdG9JckhnRzZQTm45aGd0V0kvMlQ3Y3c"));
 
-        HashMap<String, String> values = new HashMap();
+        HashMap<String, String> values = new HashMap<>();
         values.put("accountHolderName", "Mr Buliva!");
 //        values.put("accountHolderNamQe", "Mr Buliva!");
         values.put("accountNumber", "1234567890");
